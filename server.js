e4f4c71ce1b961b7a6963bca0de84c92d29c18bc
junaidahmed93@live.com <aries1993>
2016-04-01 22:43:25 +0500

    Server Established with Express

---
+++ server.js
@@ -0,0 +1,17 @@
+/// <reference path="./typings/tsd.d.ts" />
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+app.set("port", 5000);
+var staticDIR = path.resolve(__dirname, "./static");
+app.use(express.static(staticDIR));
+app.use(bodyParser.json());
+app.get('*', function (req, res) {
+    var indexViewPath = path.resolve(__dirname, './static/web/index.html');
+    res.sendFile(indexViewPath);
+    ;
+});
+app.listen(app.get("port"), function () {
+    console.log("server started at ", app.get("port"));
+});
---
+++ server.ts
@@ -0,0 +1,23 @@
+/// <reference path="./typings/tsd.d.ts" />
+
+import express = require('express');
+import path = require('path');
+import bodyParser = require('body-parser');
+
+let app = express();
+
+app.set("port",5000);
+
+let staticDIR = path.resolve(__dirname , "./static");
+app.use(express.static(staticDIR));
+app.use(bodyParser.json());
+
+app.get('*',(req:express.Request, res:express.Response)=>{
+    let indexViewPath = path.resolve(__dirname , './static/web/index.html');
+    res.sendFile(indexViewPath);
+;});
+
+
+app.listen(app.get("port"),()=>{
+   console.log("server started at " , app.get("port")); 
+});
